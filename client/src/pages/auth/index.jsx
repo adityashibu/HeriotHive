ce65c2b57d4dff850b476ce1b54c0508e8d44691
Aditya Shibu <as2397@hw.ac.uk>
2024-08-04 23:20:11 +0400

    Added some basic UI

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,5 +1,17 @@
 const Auth = () => {
-    return <div>Auth</div>
+    return <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+        <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] rounded-3xl md:w-[90vw] lg:w-[70vw] xl:w-[60vw] grid">
+            <div className="flex flex-col gap-10 items-center justify-center">
+                <div className="flex items-center justify-center flex-col">
+                    <div className="flex items-center justify-center h-full w-full">
+                        <h1 className="text-5xl font-bold md:text-6xl">
+                            Welcome to Heriot Hive
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 };
 
 export default Auth;
