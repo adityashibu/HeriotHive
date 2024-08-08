ff936b9a042c732ac48901d030aec616060c0e73
Aditya Shibu <as2397@hw.ac.uk>
2024-08-08 21:39:40 +0400

    Server files

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -3,14 +3,21 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import apiClient from "@/lib/api-client";
-import { SIGNUP_ROUTE } from "@/utils/constants";
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants";
 import { useState } from "react";
 
 import Lottie from "react-lottie";
+import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 
 
 const Auth = () => {
+    const navigate = useNavigate
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+
     const defaultOptions = {
         autoplay: true,
         animationData: animatedH,
@@ -19,9 +26,17 @@ const Auth = () => {
         }
     };
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
+    const validateLogin = () => {
+        if (!email.length) {
+            toast.error("Email is required");
+            return false;
+        }
+        if (!password.length) {
+            toast.error("Password is required");
+            return false;
+        }
+        return true;
+    }
 
     const validateSignup = () => {
         if (!email.length) {
@@ -40,15 +55,31 @@ const Auth = () => {
     };
 
     const handleLogin = async () => {
-
+        if (validateLogin()) {
+            const response = await apiClient.post(LOGIN_ROUTE,
+                { email, password },
+                { withCredentials: true }
+            );
+            if (response.data.user.id) {
+                if (response.data.user.profileSetup) navigate("/chat");
+                else navigate("/profile");
+            }
+            console.log({ response });
+        }
     }
 
     const handleSignup = async () => {
         if (validateSignup()) {
-            const response = await apiClient.post(SIGNUP_ROUTE, { email, password });
+            const response = await apiClient.post(SIGNUP_ROUTE,
+                { email, password },
+                { withCredentials: true }
+            );
+            if (response.status === 201) {
+                navigate("/profile");
+            }
             console.log(response);
         }
-    }
+    };
 
     return <div className="h-[100vh] w-[100vw] flex items-center justify-center bg-white">
         <div className="sm:h-[80vh] sm:w-[80vw] md:w-[90vw] md:h-[90vh] lg:w-[70vw] xl:w-[60vw] bg-customBlue border border-customBlue border-opacity-90 shadow-5xl rounded-3xl p-8 grid sm:grid-cols-1 xl:grid-cols-2 bg-opacity-30">
@@ -62,7 +93,7 @@ const Auth = () => {
                     <p className="font-medium text-center text-black text-[12px] md:text-lg">Fill in the details to get started with the best chat app in Heriot</p>
                 </div>
                 <div className="flex items-center justify-center w-full">
-                    <Tabs className="w-3/4">
+                    <Tabs className="w-3/4" defaultValue="login">
                         <TabsList className="bg-transparent rounded-none w-full">
                             <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-customBlue data-[state=active]:border-b-customBlue p-3 transition-all duration-300">Log In</TabsTrigger>
                             <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-customBlue data-[state=active]:border-b-customBlue p-3 transition-all duration-300">Sign Up</TabsTrigger>
