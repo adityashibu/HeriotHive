824d80eb2ffbfa88f3ee2623c3cded38a197e12f
Aditya Shibu <as2397@hw.ac.uk>
2024-08-13 23:24:42 +0400

    Connected database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 
 const app = express();
@@ -12,4 +13,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
