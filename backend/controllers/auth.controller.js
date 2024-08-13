1140ff63bf76a69be5361055b72fc5b8d88f4ce2
Aditya Shibu <as2397@hw.ac.uk>
2024-08-14 00:08:52 +0400

    Completed connection cycle

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,54 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
     try {
-        const { fullName, username, password, confirmPassword, gender } = req.body;
-    } catch (error) {
+        const { firstName, lastName, username, password, confirmPassword, gender } = req.body;
 
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Password and Confirm Password do not match" });
+        };
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "User already exists" });
+        };
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const profilePic = `https://avatar.iran.liara.run/username?username=${firstName}+${lastName}`;
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic,
+        });
+
+        if (newUser) {
+            // Generate JWT token
+
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller:", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-});
+dotenv.config();
 
+app.use(express.json()); // To parse JSON data (from req.body)
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     res.send("Server is ready");
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
