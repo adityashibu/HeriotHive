34ed974e007466386355d4136cf054f0a65b32a6
Aditya Shibu <as2397@hw.ac.uk>
2024-08-14 22:19:33 +0400

    Completed get Messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -29,6 +29,8 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         };
 
+        // Socket IO Functionality
+
         // This will run in parallel
         await Promise.all([conversation.save(), newMessage.save()]);
 
@@ -36,6 +38,23 @@ export const sendMessage = async (req, res) => {
 
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages);
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
