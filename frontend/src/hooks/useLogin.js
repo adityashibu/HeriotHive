c16df35ee5150e021af80687ac50c85ac4e6d1f5
Aditya Shibu <as2397@hw.ac.uk>
2024-08-17 10:54:43 +0400

    Completed login functionality completely

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -7,6 +7,10 @@ const useLogin = () => {
     const { setAuthUser } = useAuthContext();
 
     const login = async (username, password) => {
+
+        const success = handleInputErrors(username, password);
+        if (!success) return;
+
         setLoading(true);
         try {
             const res = await fetch("/api/auth/login", {
@@ -45,4 +49,45 @@ const useLogin = () => {
     return { loading, login };
 };
 
-export default useLogin
+export default useLogin;
+
+function handleInputErrors(username, password) {
+    if (!username) {
+        toast('Please fill in the Username',
+            {
+                icon: '🚨',
+                style: {
+                    borderRadius: '10px',
+                    background: '#333',
+                    color: '#0470e4',
+                },
+            }
+        );
+        return false;
+    } else if (!password) {
+        toast('Please fill in the Password',
+            {
+                icon: '🚨',
+                style: {
+                    borderRadius: '10px',
+                    background: '#333',
+                    color: '#0470e4',
+                },
+            }
+        );
+        return false;
+    } else if (password.length < 8) {
+        toast('Password must be at least 8 characters long',
+            {
+                icon: '🚨',
+                style: {
+                    borderRadius: '10px',
+                    background: '#333',
+                    color: '#0470e4',
+                },
+            }
+        );
+        return false;
+    }
+    return true;
+}
