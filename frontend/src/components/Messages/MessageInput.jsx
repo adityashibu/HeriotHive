66f9726fb36b28538f53a07ec78fc3a370ca46a6
Aditya Shibu <as2397@hw.ac.uk>
2024-08-17 14:42:43 +0400

    Completed Send Messahes

--- frontend/src/components/Messages/MessageInput.jsx
+++ frontend/src/components/Messages/MessageInput.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { BsSend } from "react-icons/bs";
+import useSendMessage from "../../hooks/useSendMessage";
 
 const MessageInput = () => {
+    const [message, setMessage] = useState("");
+    const { loading, sendMessage } = useSendMessage()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!message) return;
+
+        await sendMessage(message);
+        setMessage("");
+    }
+
     return (
-        <form className='px-4 my-3'>
+        <form className='px-4 my-3' onSubmit={handleSubmit}>
             <div className='w-full relative'>
                 <input
                     type='text'
                     className='border border-primaryBlue text-sm rounded-lg block w-full p-2.5  bg-gray-700 text-white'
                     placeholder='Send a message'
+                    onChange={(e) => setMessage(e.target.value)}
                 />
                 <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-                    <BsSend className="text-white hover:text-primaryBlue transition-colors duration-300 ease-in-out" />
+                    {loading ? <div className="loading loading-spinner"></div> : <BsSend className="text-white hover:text-primaryBlue transition-colors duration-300 ease-in-out" />}
                 </button>
             </div>
         </form>
---
+++ frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import useConversation from "../store/useConversation";
+import toast from "react-hot-toast";
+
+const useSendMessage = () => {
+
+    const [loading, setLoading] = useState(false);
+    const { messages, setMessages, selectedConversation } = useConversation();
+
+    const sendMessage = async (message) => {
+        setLoading(true);
+        try {
+            const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ message }),
+            });
+            const data = await res.json();
+            if (data.error) {
+                throw new Error(data.error);
+            }
+
+            setMessages([...messages, data]);
+        } catch (error) {
+            toast(error.message,
+                {
+                    icon: '🚨',
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#0470e4',
+                    },
+                }
+            );
+        } finally {
+            setLoading(false);
+        };
+    };
+
+    return { sendMessage, loading };
+}
+
+export default useSendMessage
